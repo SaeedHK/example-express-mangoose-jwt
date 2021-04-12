286793f14fb783ec4e1824f61ae617ce1b054eba
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-12 10:14:04 +0200

    New model: User

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  // unique Ids are generated automotically by mongoose
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
