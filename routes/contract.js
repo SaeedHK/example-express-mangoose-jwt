5a5cc15fd9ada2026bbc88c73f5f9909d092703d
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-12 10:34:27 +0200

    Add new endpoint to set contract finish date

--- routes/contract.js
+++ routes/contract.js
@@ -59,4 +59,27 @@ router.post('/', auth, async (req, res) => {
   }
 });
 
+// Finish contract. Only admins or contract clients are allowed
+router.put('/finish', auth, async (req, res) => {
+  try {
+    const { contractId, finishAt } = req.body;
+    const contract = await Contract.findById(contractId);
+    if (!contract) {
+      res.status(400).send(`There is no contract with id ${contractId}`);
+    }
+    // Only admins or contract clients can finish it
+    else if (req.userIsAdmin || contract.clients.includes(req.userId)) {
+      await contract.update({ finishAt: formatDate(finishAt) });
+      res.status(200).send(`Set finish date contract with id ${contract._id}`);
+    } else {
+      res
+        .status(400)
+        .send('Only contract administrators or clients are allowed to terminate contracts');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send('Finish contract failed');
+  }
+});
+
 module.exports = router;
