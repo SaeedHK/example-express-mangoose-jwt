31afa783ad635e665e9f2d5820ca19d3ec9c394c
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-12 10:30:01 +0200

    Add new route to get contracts by id

--- routes/contract.js
+++ routes/contract.js
@@ -18,4 +18,24 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// Get contract by ID
+router.get('/:contractId', auth, async (req, res) => {
+  try {
+    const { contractId } = req.params;
+    const contract = Contract.findById(contractId);
+    if (!contract) {
+      res.status(400).send(`There is no contract with id ${contractId}`);
+    } else if (req.userIsAdmin) {
+      res.status(200).send(contract);
+    } else if (contract.clients.includes(req.userId)) {
+      res.status(200).send(contract);
+    } else {
+      res.status(400).send('This user is not allowed to access this contract');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send('Get contract failed');
+  }
+});
+
 module.exports = router;
