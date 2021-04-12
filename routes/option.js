86b5e10386548ed96ca318b4c610ba4b8135d305
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-12 10:38:37 +0200

    Add new endpoint to get options by id

--- routes/option.js
+++ routes/option.js
@@ -17,4 +17,20 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// Get Option by id
+router.get('/:optionId', auth, async (req, res) => {
+  try {
+    const { optionId } = req.params;
+    const option = await Option.findById(optionId);
+    if (option) {
+      res.status(200).send(option);
+    } else {
+      res.status(400).send(`There is no option with id ${optionId}`);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send('Getting option failed');
+  }
+});
+
 module.exports = router;
