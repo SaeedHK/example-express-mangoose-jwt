d999e2336b0715a9f48d9ead5eaad46fb5c7f9e1
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-12 10:22:21 +0200

    Add user/sginup endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const auth = require('../middleware/auth');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 // Get all users (only allowed to admin)
@@ -9,6 +10,31 @@ router.get('/', auth, async (req, res) => {
     res.status(200).send(users);
   } else {
     res.status(400).send('Only admin can see list of users');
+  }
+});
+
+// Signup non-admin users
+router.post('/signup', async (req, res) => {
+  // TODO: Validate body fields (use for example joi)
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send('Both email and password are obligatory');
+  } else {
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      res.status(400).send(`User with email ${email} already exists`);
+    } else {
+      try {
+        // Encrypt password to register to db
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        await User.create({ email, password: hashedPassword });
+        res.status(200).send(`Create user with email ${email}`);
+      } catch (err) {
+        console.log(err);
+        res.status(400).send(`Failed creation user with email ${email}`);
+      }
+    }
   }
 });
 
