6a055b8c7761580097fcb19044a5c87298501540
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-12 10:12:47 +0200

    Init express server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// Load env variables from .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Connect to DB
+const connectDB = require('./db');
+connectDB();
+
+// Initialize App
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`Server is up and running on ${PORT}.`));
