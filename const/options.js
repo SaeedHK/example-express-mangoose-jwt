de220c541b7f3c55961c9a19457f561b545d240e
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-14 14:44:39 +0200

    Add options

---
+++ const/options.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { title: 'tout risque', description: 'tout risque' },
+  { title: 'vol uniquement', description: 'vol uniquement' },
+  { title: 'cambriolé un vendredi', description: 'cambriolé un vendredi' },
+  { title: 'incendie uniquement', description: 'incendie uniquement' },
+];
